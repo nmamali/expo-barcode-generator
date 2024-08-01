d2ea77da9b376dcef28888ca2de37019f2419f93
JP Strydom <jp.strydom@entelect.co.za>
2024-08-01 14:13:33 +0200

    Added Expo 51 support

--- components/BarcodeChunk.jsx
+++ components/BarcodeChunk.jsx
@@ -1,58 +1,57 @@
-import {Rect} from "react-native-svg";
-import PropTypes from "prop-types";
+import { Rect } from 'react-native-svg';
+import PropTypes from 'prop-types';
 
-export const BarcodeChunk = ({binary, padding, options})=> {
+export const BarcodeChunk = ({ binary, padding, options }) => {
+  // Creates the barcode out of the encoded binary
+  let yFrom;
+  if (options.textPosition == 'top') {
+    yFrom = options.fontSize + options.textMargin;
+  } else {
+    yFrom = 0;
+  }
 
-    // Creates the barcode out of the encoded binary
-    let yFrom;
-    if (options.textPosition == 'top') {
-        yFrom = options.fontSize + options.textMargin;
-    } else {
-        yFrom = 0;
+  let barWidth = 0;
+  let x = 0;
+  const bars = [];
+  for (let b = 0; b < binary.length; b++) {
+    x = b * options.width + padding;
+
+    if (binary[b] === '1') {
+      barWidth++;
+    } else if (barWidth > 0) {
+      bars.push({
+        x: x - options.width * barWidth,
+        y: yFrom,
+        width: options.width * barWidth,
+        height: options.height
+      });
+      barWidth = 0;
     }
+  }
 
-    let barWidth = 0;
-    let x = 0;
-    const bars = [];
-    for (let b = 0; b < binary.length; b++) {
-        x = b * options.width + padding;
+  // Last draw is needed since the barcode ends with 1
+  if (barWidth > 0) {
+    bars.push({
+      x: x - options.width * (barWidth - 1),
+      y: yFrom,
+      width: options.width * barWidth,
+      height: options.height
+    });
+  }
 
-        if (binary[b] === '1') {
-            barWidth++;
-        } else if (barWidth > 0) {
-            bars.push({
-                x: x - options.width * barWidth,
-                y: yFrom,
-                width: options.width * barWidth,
-                height: options.height
-            });
-            barWidth = 0;
-        }
-    }
-
-    // Last draw is needed since the barcode ends with 1
-    if (barWidth > 0) {
-        bars.push({
-            x: x - options.width * (barWidth - 1),
-            y: yFrom,
-            width: options.width * barWidth,
-            height: options.height
-        });
-    }
-
-    return bars.map((bar, i) => (
-        <Rect key={i} x={bar.x} y={bar.y} width={bar.width} height={bar.height} />
-    ));
-}
+  return bars.map((bar, i) => (
+    <Rect key={i} x={bar.x} y={bar.y} width={bar.width} height={bar.height} />
+  ));
+};
 
 BarcodeChunk.propTypes = {
-    padding: PropTypes.number,
-    binary: PropTypes.arrayOf(PropTypes.string),
-    options: PropTypes.shape({
-        textPosition: PropTypes.oneOf(["top", "bottom"]),
-        fontSize: PropTypes.number,
-        textMargin: PropTypes.number,
-        width: PropTypes.number,
-        height: PropTypes.number,
-    }),
+  padding: PropTypes.number,
+  binary: PropTypes.arrayOf(PropTypes.string),
+  options: PropTypes.shape({
+    textPosition: PropTypes.oneOf(['top', 'bottom']),
+    fontSize: PropTypes.number,
+    textMargin: PropTypes.number,
+    width: PropTypes.number,
+    height: PropTypes.number
+  })
 };
--- components/BarcodeText.jsx
+++ components/BarcodeText.jsx
@@ -1,59 +1,59 @@
-import {Text} from "react-native-svg";
+import { Text } from 'react-native-svg';
 import PropTypes from 'prop-types';
 
-export const BarcodeText = ({text, width, padding, options}) => {
-    // Draw the text if displayValue is set
-    if (options.displayValue) {
-        let x, y, textAnchor;
+export const BarcodeText = ({ text, width, padding, options }) => {
+  // Draw the text if displayValue is set
+  if (options.displayValue) {
+    let x, y, textAnchor;
 
-        if (options.textPosition == 'top') {
-            y = options.fontSize - options.textMargin;
-        } else {
-            y = options.height + options.textMargin + options.fontSize;
-        }
-
-        // Draw the text in the correct X depending on the textAlign option
-        if (options.textAlign == 'left' || padding > 0) {
-            x = 0;
-            textAnchor = 'start';
-        } else if (options.textAlign == 'right') {
-            x = width - 1;
-            textAnchor = 'end';
-        }
-        // In all other cases, center the text
-        else {
-            x = width / 2;
-            textAnchor = 'middle';
-        }
-
-        return (
-            <Text
-                x={x}
-                y={y}
-                fontSize={options.fontSize}
-                fontWeight="bold"
-                textAnchor={textAnchor}
-                fill={options.lineColor}
-            >
-                {text}
-            </Text>
-        );
+    if (options.textPosition == 'top') {
+      y = options.fontSize - options.textMargin;
     } else {
-        return null;
+      y = options.height + options.textMargin + options.fontSize;
     }
+
+    // Draw the text in the correct X depending on the textAlign option
+    if (options.textAlign == 'left' || padding > 0) {
+      x = 0;
+      textAnchor = 'start';
+    } else if (options.textAlign == 'right') {
+      x = width - 1;
+      textAnchor = 'end';
+    }
+    // In all other cases, center the text
+    else {
+      x = width / 2;
+      textAnchor = 'middle';
+    }
+
+    return (
+      <Text
+        x={x}
+        y={y}
+        fontSize={options.fontSize}
+        fontWeight="bold"
+        textAnchor={textAnchor}
+        fill={options.lineColor}
+      >
+        {text}
+      </Text>
+    );
+  } else {
+    return null;
+  }
 };
 
 BarcodeText.propTypes = {
-    text: PropTypes.string,
-    width: PropTypes.number,
-    padding: PropTypes.number,
-    options: PropTypes.shape({
-        displayValue: PropTypes.bool,
-        textPosition: PropTypes.oneOf(['top', 'bottom']),
-        fontSize: PropTypes.number,
-        textMargin: PropTypes.number,
-        textAlign: PropTypes.oneOf(['left', 'right', 'center']),
-        height: PropTypes.number,
-        lineColor: PropTypes.string,
-    })
+  text: PropTypes.string,
+  width: PropTypes.number,
+  padding: PropTypes.number,
+  options: PropTypes.shape({
+    displayValue: PropTypes.bool,
+    textPosition: PropTypes.oneOf(['top', 'bottom']),
+    fontSize: PropTypes.number,
+    textMargin: PropTypes.number,
+    textAlign: PropTypes.oneOf(['left', 'right', 'center']),
+    height: PropTypes.number,
+    lineColor: PropTypes.string
+  })
 };
